801827c22ee00d215587cdd2f21dc510e609157b
vuvohoang1823 <vuvohoang1823@gmail.com>
2024-05-20 20:12:36 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-5xl font-bold text-center items-center justify-center text-blue-500 mt-[300px]">
+      LOLOCA
+    </h1>
+  )
+}
